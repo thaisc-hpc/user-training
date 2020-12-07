e0e1135ddcd56111f57f52b92b6389b5401c841c
Viwan J <viwan.jrrt@gmail.com>
2020-12-07 16:10:22 +0700

    Update submit.sh

--- SIN101-Singularity_On_HPC101/Working_with_Singualrity/Customizing_Container/submit.sh
+++ SIN101-Singularity_On_HPC101/Working_with_Singualrity/Customizing_Container/submit.sh
@@ -8,21 +8,23 @@
 
 module purge                        # unload all modules as they may have previously been loaded.
 
-# uncomment the below line to use Singularity
-#module load Singularity                   # Load the module that you want to use. This example is intel
-
 # uncomment the below line to use Python
 #module load Python/3.7.4-GCCcore-8.3.0
 
-# Run your program or executable code
-# uncomment specific lines to use an appropriate commands
+# uncomment the below line to use Singularity
+#module load Singularity                   # Load the module that you want to use. This example is intel
+
+# ------  Run your program or executable code ------
+# uncomment set of specific lines to use an appropriate commands
+#option 1 : using 
 #python check_opencv2.py
 
-# Singularity command
-# singularity exec [SIF] [command] [command options]
-
+#option 2 : using 
 #echo "Run using original Tensorflow image (tensorflow_nightly-gpu.sif)"
 #singularity exec tensorflow_nightly-gpu.sif python3 check_opencv2.py
 
+#option 3 : using 
 #echo "Run using customized Tensorflow image (my_TF.sif)"
 #singularity exec my_TF.sif python3 check_opencv2.py
+
+# Note : Singularity command : singularity exec [SIF] [command] [command options]
